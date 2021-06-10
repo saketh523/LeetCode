2ea141123de93da8114e974f4f174d5b742f14cb
Saketh Jammula <jammulasakethreddy@gmail.com>
2021-06-11 05:10:28 +0530

    1837. Sum of Digits in Base K

---
+++ SumBase.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sumBase(int n, int k) { 
+        int res = 0;
+        while(n > 0){
+                    res = res + n%k;
+                    n = n/k;
+                }
+        return res;
+    }
+}
